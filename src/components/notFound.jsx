fe2f0fd2408f17e330055459114e1947b64105a2
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-15 23:13:13 +0930

    Not found component updated.

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFound = () => {
-  return <h1>Page not found...</h1>;
+  return <h2 style={{ marginTop: '50px'}}>Page not found...</h2>;
 };
 
 export default NotFound;
