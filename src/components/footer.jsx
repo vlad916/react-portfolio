63368e4ba05cf4453aa376684935ff52e6557443
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-15 22:40:11 +0930

    Contact component updated.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,30 +5,28 @@ import './css/footer.css';
 
 const Footer = () => {
   return (
-   
-      <footer>
-        <div className="row">
-          <div className="container-fluid text-center p-2">
-            <a href="mailto:ungosangelo916@yahoo.com.au">
-              <i className="far fa-envelope fa-2x mx-3"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/angelo-ungos-064619197/">
-              <i className="fab fa-linkedin-in fa-2x mx-3"></i>
-            </a>
-            <a href="https://github.com/vlad916">
-              <i className="fab fa-github fa-2x mx-3"></i>
-            </a>
-          </div>
+    <footer>
+      <div className="row">
+        <div className="container-fluid text-center p-2">
+          <a href="mailto:ungosangelo916@yahoo.com.au">
+            <i className="far fa-envelope fa-2x mx-3"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/angelo-ungos-064619197/">
+            <i class="fab fa-linkedin-in fa-2x mx-3"></i>
+          </a>
+          <a href="https://github.com/vlad916">
+            <i class="fab fa-github fa-2x mx-3"></i>
+          </a>
         </div>
-        <div className="row">
-          <div className="container-fluid text-center">
-            <p className="copy">
-              <span>&#169;</span> 2019 Angelo Ungos
-            </p>
-          </div>
+      </div>
+      <div class="row">
+        <div class="container-fluid text-center">
+          <p id="copy">
+            <span>&#169;</span> 2019 Angelo Ungos
+          </p>
         </div>
-      </footer>
-
+      </div>
+    </footer>
   );
 };
 
